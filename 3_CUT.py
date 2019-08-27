eb89b44166c9cbad8854b0567715647349c33d73
Davide Del Vento <davide.del.vento@gmail>
2019-08-26 19:34:27 -0600

    Cut script for Mate and Gnome

---
+++ 3_CUT.py
@@ -0,0 +1,6 @@
+ac = window.get_active_class()
+if (ac == 'mate-terminal.Mate-terminal' or
+    ac == 'gnome-terminal-server.Gnome-terminal'):
+    keyboard.send_keys("<ctrl>+<shift>+c")
+else:
+    keyboard.send_keys("<ctrl>+x")
